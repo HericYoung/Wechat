0349393defc9d9d5d5c161b8d07c5b2557d6ed70
H <1332454954@qq.com>
2018-05-29 17:46:54 +0800

    增加更新提醒

--- MiniProgram/app.js
+++ MiniProgram/app.js
@@ -21,7 +21,32 @@ App({
    * @return {[type]}   [description]
    */
   onLaunch: function () {
-    
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log("有更新？" + res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+            console.log("更新完毕");
+          }
+        }
+      })
+
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+      console.log("新版本下载失败");
+    })
   },
 
   /**
@@ -36,6 +61,7 @@ App({
    */
   onShow: function () {
     // console.log('App Show')
+    
   },
 
   /**
